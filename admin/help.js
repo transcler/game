672e70357515233508765f0b6ea9f753c9e40012
transcler <transcler53@gmail.com>
2022-07-08 14:04:37 +0000

    botgame

---
+++ admin/help.js
@@ -0,0 +1,56 @@
+exports.allmenu = (prefix, namaowner) => {
+	return `[ *GAME-BOT[0]* ]
+
+~> [ *MENU* ]
+> *${prefix}login*
+> *${prefix}runtime*
+> *${prefix}speed*
+> *${prefix}donate*
+> *${prefix}sticker*
+
+~> [ *OWNER* ]
+> *${prefix}bc*
+> *${prefix}join*
+> *${prefix}leave*
+
+~> [ *GAME* ]
+> *${prefix}buykarakter*
+> *${prefix}duel*
+> *${prefix}berburu*
+> *${prefix}jualburuan*
+> *${prefix}buyenergi*
+> *${prefix}gachaeasy*
+> *${prefix}gachamedium*
+> *${prefix}gachahard*
+> *${prefix}updatehome*
+> *${prefix}siapaaku*
+> *${prefix}susunkata*
+> *${prefix}tebakkalimat*
+> *${prefix}tebakkata*
+> *${prefix}tebaklirik*
+> *${prefix}tebakkimia*
+> *${prefix}tebaktebakan*
+> *${prefix}tekateki*
+> *${prefix}asahotak* 
+> *${prefix}kapankah* 
+> *${prefix}bisakah* 
+> *${prefix}kenapa* 
+> *${prefix}dimana* 
+
+~> [ *MY-CEK* ]
+> *${prefix}myenergi*
+> *${prefix}myburuan*
+> *${prefix}myhome*
+> *${prefix}mykarakter*
+> *${prefix}mylevel*
+> *${prefix}myexp*
+> *${prefix}myuang*
+
+> [ *THX TO* ]
+• *ADIWAJSHING*
+• *MEGAWATI* 
+• *RIMURUBOTZ*
+• *LORD R1YNZ*
+• *ARIASU*
+• *${namaowner}*`
+}
